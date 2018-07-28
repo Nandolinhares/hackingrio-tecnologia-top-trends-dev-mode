abbc4c4db32ac11fead4f0b5a02e608472cede42
imbrito <ismael.20rj@gmail.com>
2018-07-28 14:55:24 -0300

    add validates

--- app/models/fisica.rb
+++ app/models/fisica.rb
@@ -1,2 +1,5 @@
 class Fisica < ApplicationRecord
+    validates :nome, :cpf, :telefone, :endereco, presence: true
+    validates_length_of :cpf, :is => 11, :message => "CPF é composto por 11 dígitos. Informe apenas os Números"
+    validates_uniqueness_of :cpf
 end
--- app/models/juridica.rb
+++ app/models/juridica.rb
@@ -1,2 +1,5 @@
 class Juridica < ApplicationRecord
+    validates :nome, :cnpj, :razao, :fantasia, :endereco, presence: true
+    validates_length_of :cnpj, :is => 11, :message => "CNPJ é composto por 14 dígitos. Informe apenas os Números"
+    validates_uniqueness_of :cnpj
 end
