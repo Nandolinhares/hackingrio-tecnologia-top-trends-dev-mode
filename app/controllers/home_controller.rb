c759ba6ddd5538bfa7d3e69f750666de9a617a3e
imbrito <ismael.20rj@gmail.com>
2018-07-29 08:49:57 -0300

    add API controller search?token

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,12 @@ class HomeController < ApplicationController
 	def index
 		
 	end
+
+	def search
+		if params[:token].present?
+		  payload = JWT.decode params[:token], 'HS256'
+		end
+		render json: payload, status: :ok
+	  end
+	
 end
